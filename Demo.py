168436a6e893ce9b41b259c3ccf72beb93d80806
Bharath Kalyan S <sbharath35@gmail.com>
2021-02-13 17:59:27 +0530

    Commit

--- Demo.py
+++ Demo.py
@@ -1,2 +1,26 @@
 # Learning Basics of OpenCV
 # Resources provided by Tech with Tim youtube Channel
+
+import cv2
+
+print('Hello I am Learning OpenCV')
+
+#To import or load a Picture we have to use read method of cv2 class.
+
+img = cv2.imread('Assets/1.jpeg')
+
+# -1 :: cv2.IMREAD_COLOR : ANY TRANSPARENT IMAGE IS LOADED NEGLECTING THE TRANSPARENCY.
+# 0 ::  cv2.IMREAD_GRAYSCALE : IN GRAYSCALE MODE
+# 1 :: cv2.IMREAD_UNCHANGED : INCLUDING ALPHA CHANNEL
+
+
+
+
+
+
+
+
+
+
+
+
