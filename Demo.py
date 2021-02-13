eee15e264ca1f93fe92a309e75d131a1ee3d2409
Bharath Kalyan S <sbharath35@gmail.com>
2021-02-13 18:11:45 +0530

    Commit

--- Demo.py
+++ Demo.py
@@ -3,24 +3,17 @@
 
 import cv2
 
-print('Hello I am Learning OpenCV')
+# To import or load a Picture we have to use imread method of cv2 class.
 
-#To import or load a Picture we have to use read method of cv2 class.
+img = cv2.imread('Assets/1.jpeg', -1)
 
-img = cv2.imread('Assets/1.jpeg')
-
+img = cv2.resize(img, (800, 400))
 # -1 :: cv2.IMREAD_COLOR : ANY TRANSPARENT IMAGE IS LOADED NEGLECTING THE TRANSPARENCY.
 # 0 ::  cv2.IMREAD_GRAYSCALE : IN GRAYSCALE MODE
 # 1 :: cv2.IMREAD_UNCHANGED : INCLUDING ALPHA CHANNEL
 
 
-
-
-
-
-
-
-
-
-
-
+cv2.imshow('Image Viewer', img)
+cv2.waitKey(0)
+# WaitKey Says that you have to wait n sec's to press a key
+cv2.destroyAllWindows()
